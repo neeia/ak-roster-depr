f20e4e352fe497b0ebfc617c3bb6eb5d3cc43a72
neia <51728998+pettankorori@users.noreply.github.com>
2021-05-05 16:33:26 -0700

    Update imports to include components directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 
-import OperatorDataTable, { OperatorProps } from './OperatorDataTable';
-import OpForm from './OpForm';
+import OperatorDataTable, { OperatorProps } from './components/OperatorDataTable';
+import OpForm from './components/OpForm';
 
 
 const angelinaProp : OperatorProps = {
