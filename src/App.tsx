634730f7a7504109d4823ab9c75cb8785f6917bb
neia <51728998+pettankorori@users.noreply.github.com>
2021-05-07 04:03:25 -0700

    Change old props to new Operator format incl. changing Op...Row props to [Operator, onChange] generating list of ops in App from json as a Record using said record to send data to Op...Rows rewiring Op...Rows inputs to use singular onChange function

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Container,
   createMuiTheme,
@@ -8,7 +8,7 @@ import {
 import "./App.css";
 
 import OperatorDataTableRow from "./components/OperatorDataTableRow";
-import operators from "./data/operators.json";
+import operatorJson from "./data/operators.json";
 
 
 const darkTheme = createMuiTheme({
@@ -17,18 +17,55 @@ const darkTheme = createMuiTheme({
   },
 });
 
+export interface Operator {
+ name : string;
+ rarity : number;
+ potential : number;
+ promotion : number;
+ owned: boolean;
+ level : number;
+ skillLevel : number;
+ skill1Mastery?: number;
+ skill2Mastery?: number;
+ skill3Mastery?: number;
+}
+
 function App() {
+  const [operators, setOperators] = useState<Record<string, Operator>>(
+    Object.fromEntries(
+      operatorJson.map((op) => [
+        op.name, 
+        {
+          name: op.name,
+          rarity: op.rarity,
+          owned: false,
+          potential: 0,
+          promotion: 0,
+          level: 0,
+          skillLevel: 0
+          }]))
+  );
+
+  const handleChange = (
+    operatorName: string,
+    property: string,
+    value: number | boolean) => {
+      const copyOperators = {...operators};
+      const operatorData = copyOperators[operatorName];
+      // operatorData[property] = value;
+      setOperators(copyOperators);
+    };
+
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <Container>
-        {operators.map((op) =>
+        {operatorJson.map((op) =>
           {
-            const props = {
-              name: op.name,
-              rarity: op.rarity
-            }
-            return <OperatorDataTableRow {... props} />;
+            const opData = operators[op.name];
+            return <OperatorDataTableRow 
+                    operator={opData} 
+                    onChange = {handleChange} />;
           })
         }
       </Container>
--- src/components/OperatorDataTableRow.tsx
+++ src/components/OperatorDataTableRow.tsx
@@ -2,27 +2,20 @@ import { Grid } from "@material-ui/core";
 import React from "react";
 import slugify from "slugify";
 import { useState } from "react";
+import { Operator } from "../App";
 
 interface Props {
-  name: string;
-  rarity: number;
+  operator: Operator;
+  onChange: (operatorName: string, property: string, value: number | boolean) => void;
 }
 
 function OperatorDataTableRow(props: Props) {
-  const { name, rarity } = props;
-  let potential = 0;
-  const [promotion, setPromotion] = useState(0);
-  let level = 0;
-  const [skillLevel, setSkillLevel] = useState(0);
-  let skill1Mastery = 0;
-  let skill2Mastery = 0;
-  let skill3Mastery = 0;
-  const [owned, setOwned] = useState(false);
+  const { operator, onChange } = props;
   
-  let intermediate = name;
-  if (promotion === 2) {
+  let intermediate = operator.name;
+  if (operator.promotion === 2) {
     intermediate += " elite 2";
-  } else if (promotion === 1 && name === "Amiya") {
+  } else if (operator.promotion === 1 && operator.name === "Amiya") {
     intermediate += " elite 1";
   }
 
@@ -31,51 +24,88 @@ function OperatorDataTableRow(props: Props) {
     { lower: true, replacement: "-" }
   )}`;
 
-
-  const onChange = (opName:string, propName:string, propValue:any) => {
-
-  };
-
   return (
     <Grid container spacing={2} alignItems="center">
       <Grid item>
         <input 
-          type="checkbox" 
           name="owned"
-          checked={owned} 
-          onChange={(e) => onChange(name, e.target.name, e.target.checked)}
+          type="checkbox" 
+          checked={operator.owned} 
+          onChange={(e) => onChange(operator.name, e.target.name, e.target.checked)}
         />
       </Grid>
       <Grid item>
         <img
-          style={{opacity :  (owned ? 1 : 0.2)}}
+          style={{opacity :  (operator.owned ? 1 : 0.2)}}
           className="table-icon-small" 
           src={imgUrl} 
-          alt={name} />
+          alt={operator.name} />
       </Grid>
-      <Grid item>{rarity}</Grid>
-      <Grid item>{name}</Grid>
-      <Grid item><input defaultValue={potential} disabled={!owned}/></Grid>
+      <Grid item>{operator.rarity}</Grid>
+      <Grid item>{operator.name}</Grid>
+      <Grid item>
+        <input 
+          name="potential"
+          type="number"
+          value={operator.potential}
+          disabled={!operator.owned}
+          onChange={(e) => onChange(operator.name, e.target.name, e.target.valueAsNumber)} 
+        />
+      </Grid>
       <Grid item>
         <input 
           name="promotion"
-          defaultValue={promotion}
-          disabled={!owned}
-          onChange={(e) => onChange(name, e.target.name, e.target.value)} 
+          type="number"
+          value={operator.promotion}
+          disabled={!operator.owned}
+          onChange={(e) => onChange(operator.name, e.target.name, e.target.valueAsNumber)} 
         />
       </Grid>
-      <Grid item><input defaultValue={level} disabled={!owned}/></Grid>
+      <Grid item>
+        <input 
+          name="level"
+          type="number"
+          value={operator.level}
+          disabled={!operator.owned}
+          onChange={(e) => onChange(operator.name, e.target.name, e.target.valueAsNumber)} 
+        />
+      </Grid>
       <Grid item>
         <input 
           name="skillLevel"
-          defaultValue={skillLevel}
-          disabled={!owned}
-          onChange={(e) => onChange(name, e.target.name, e.target.value)} 
+          type="number"
+          value={operator.skillLevel}
+          disabled={!operator.owned}
+          onChange={(e) => onChange(operator.name, e.target.name, e.target.valueAsNumber)} 
         />
       </Grid>
-      <Grid item><input defaultValue={skill1Mastery} disabled={promotion<2 || skillLevel<7 || !owned}/></Grid>
-      <Grid item><input defaultValue={skill2Mastery} disabled={promotion<2 || skillLevel<7 || !owned}/></Grid>
-      <Grid item><input defaultValue={skill3Mastery} disabled={promotion<2 || skillLevel<7 || !owned}/></Grid>
+      <Grid item>
+        <input 
+          name="skill1Mastery"
+          type="number"
+          value={operator.skill2Mastery}
+          disabled={operator.promotion<2 || operator.skillLevel<7 || !operator.owned}
+          onChange={(e) => onChange(operator.name, e.target.name, e.target.valueAsNumber)} 
+        />
+      </Grid>
+      <Grid item>
+        <input 
+          name="skill1Mastery"
+          type="number"
+          value={operator.skill2Mastery}
+          disabled={operator.promotion<2 || operator.skillLevel<7 || !operator.owned}
+          onChange={(e) => onChange(operator.name, e.target.name, e.target.valueAsNumber)} 
+        />
+      </Grid>
+      <Grid item>
+        <input 
+          name="skill2Mastery"
+          type="number"
+          value={operator.skill2Mastery}
+          disabled={operator.promotion<2 || operator.skillLevel<7 || !operator.owned}
+          onChange={(e) => onChange(operator.name, e.target.name, e.target.valueAsNumber)} 
+        />
+      </Grid>
     </Grid>
   );
 }
