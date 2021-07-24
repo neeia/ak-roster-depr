e2cbe0f7076ddcf0ab4db19e98f22ad315866ca8
neia <51728998+pettankorori@users.noreply.github.com>
2021-07-24 11:11:56 -0700

    Update important components (validator, disable)

--- src/components/RosterTable.tsx
+++ src/components/RosterTable.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import slugify from "slugify";
 import { styled, TableCell } from "@material-ui/core";
 import {
   Table,
@@ -10,6 +11,8 @@ import {
 } from "react-virtualized";
 import { defaultSortComparator, Operator } from "../App";
 import ValidatedTextField from "./ValidatedTextField";
+
+const MAX_LEVEL_BY_RARITY = [[0], [30], [30], [40, 55], [45, 60, 70], [50, 70, 80], [50, 80, 90]];
 
 const headCells = [
   {
@@ -154,6 +157,82 @@ const RosterTable: React.FC<Props> = (props) => {
       defaultSortComparator(operators[a.id], operators[b.id])
   );
 
+
+  const BodyCell: TableCellRenderer = (props) => {
+    const {
+      cellData,
+      columnData,
+      columnIndex,
+      dataKey,
+      isScrolling,
+      rowData,
+      rowIndex,
+    } = props;
+
+    let innerNode = undefined;
+    switch (dataKey) {
+      case "icon":
+        innerNode = 
+          <img
+            style={{ opacity: rowData.owned ? 1 : 0.2 }}
+            width={48}
+            height={48}
+            src={imageUrlByOp(rowData.name, rowData.promotion)}
+            alt={rowData.name}
+          />;
+        break;
+      case "name":
+      case "rarity":
+        innerNode = cellData;
+        break;
+      case "favorite":
+      case "owned":
+        innerNode = 
+          <input 
+            type="checkbox" 
+            name={dataKey} checked={cellData} 
+            onChange={(e) =>
+              onChange(rowData.id, dataKey, e.target.checked)
+            }
+          />;
+        break;
+      case "potential":
+      case "promotion":
+      case "level":
+      case "skillLevel":
+      case "skill1Mastery":
+      case "skill2Mastery":
+      case "skill3Mastery":
+        innerNode = (
+          <ValidatedTextField
+            name={dataKey}
+            type="number"
+            value={cellData}
+            disabled={
+              disableByProperty(rowData, dataKey)
+            }
+            validator={
+              validatorForNumericProperty(dataKey, cellData.rarity, cellData.promotion)
+            }
+            onChange={(e) =>
+              onChange(rowData.id, e.target.name, +e.target.value)
+            }
+          />
+        );
+        break;
+      default:
+        throw new Error(`Unknown operator property: ${dataKey}`);
+    }
+
+    console.log(rowData);
+
+    return (
+      <StyledTableCell component="div" variant="body">
+        {innerNode}
+      </StyledTableCell>
+    );
+  };
+
   return (
     <AutoSizer disableHeight>
       {({ width }) => (
@@ -217,54 +296,68 @@ const HeaderCell: TableHeaderRenderer = (props) => {
   );
 };
 
-const BodyCell: TableCellRenderer = (props) => {
-  const {
-    cellData,
-    columnData,
-    columnIndex,
-    dataKey,
-    isScrolling,
-    rowData,
-    rowIndex,
-  } = props;
+const imageUrlByOp = (opName: string, promotion: number) => {
+  let intermediate = opName;
+  if (promotion === 2) {
+    intermediate += " elite 2";
+  } else if (promotion === 1 && opName === "Amiya") {
+    intermediate += " elite 1";
+  }
+  return `https://res.cloudinary.com/samidare/image/upload/v1/arknights/operators/${slugify(
+    intermediate,
+    { lower: true, replacement: "-", remove: /-/g }
+  )}`;
+}
 
-  let innerNode = undefined;
-  switch (dataKey) {
-    case "icon":
-      innerNode = <img />;
-      break;
-    case "name":
-    case "rarity":
-      innerNode = cellData;
-      break;
-    case "favorite":
-    case "owned":
-      innerNode = <input type="checkbox" name={dataKey} checked={cellData} />;
-      break;
+const disableByProperty = (
+  op : Operator,
+  property: string,
+) => {
+  if (!op.owned) { return false; }
+  switch (property) {
     case "potential":
-    case "promotion":
     case "level":
+      return true;
+    case "promotion":
     case "skillLevel":
+      return op.rarity > 2;
+    case "skill1Mastery":
+    case "skill2Mastery":
+      return op.promotion < 2 || op.skillLevel < 7;
+    case "skill3Mastery":
+      return op.promotion < 2 || op.skillLevel < 7 || (op.rarity < 6 && op.name !== "Amiya");
+    default:
+      throw new Error(`Unknown numeric property: ${property}`);
+  }
+};
+
+const validatorForNumericProperty = (
+  property: string,
+  rarity: number,
+  promotion: number
+) => {
+  let upper = 0;
+  switch (property) {
+    case "potential":
+      upper = 6;
+      break;
+    case "promotion":
+      upper = (rarity === 3 ? 1 : 2)
+      break;
+    case "level":
+      upper = MAX_LEVEL_BY_RARITY[rarity][promotion];
+      break;
+    case "skillLevel":
+      upper = (promotion === 0 ? 4 : 7);
+      break;
     case "skill1Mastery":
     case "skill2Mastery":
     case "skill3Mastery":
-      innerNode = (
-        <ValidatedTextField
-          name={dataKey}
-          type="number"
-          value={cellData}
-          validator={() => true}
-          onChange={() => {}}
-        />
-      );
+      upper = 3;
       break;
     default:
-      throw new Error(`Unknown operator property: ${dataKey}`);
+      throw new Error(`Unknown numeric property: ${property}`);
   }
-
-  return (
-    <StyledTableCell component="div" variant="body">
-      {innerNode}
-    </StyledTableCell>
-  );
+  return (value: string) =>
+    !Number.isNaN(+value) && 0 <= +value && +value <= upper;
 };
