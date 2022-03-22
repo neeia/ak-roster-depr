12e177e7e6e7010a842774702fb09ddbdc5716fd
Neeia <neeia2718@gmail.com>
2022-03-22 05:43:52 -0700

    Remove all absolutely and relatively positioned elements This commit fixes the performance errors in the collection tab

--- src/components/OperatorCollectionBlock.tsx
+++ src/components/OperatorCollectionBlock.tsx
@@ -1,63 +1,47 @@
-import { Box, makeStyles } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
 import React from "react";
 import slugify from "slugify";
-import { Operator, MOBILE_BREAKPOINT, TABLET_BREAKPOINT } from "../App";
+import { Operator } from "../App";
 import SkillDisplayBox from "./SkillDisplayBox";
-import useWindowSize, { Size } from "./UseWindowSize";
-import { COLOR_BY_RARITY } from "./DataTab";
+import clsx from "clsx";
 
 const useStyles = makeStyles({
-  opBox: {
-    justifyContent: "space-between",
-    backgroundColor: "#333333",
-    padding: "8px",
+  opContainer: {
+    display: "grid",
+    gridTemplateAreas: `"img name"
+                       "img info"`,
+    gridTemplateRows: "auto auto",
+    gridTemplateColumns: "auto 1fr",
+    width: "300px",
+    padding: "4px",
     border: "1px solid white",
-    borderRadius: "5px",
-    width: "236px",
-    height: "260px",
+    borderRadius: "4px",
+    backgroundColor: "#333333",
     boxShadow: "2px 2px 8px rgb(0 0 0 / 30%)",
   },
-  opBoxMobile: {
-    justifyContent: "space-between",
-    backgroundColor: "#333333",
-    padding: "8px",
-    margin: "4px",
-    border: "1px solid white",
-    borderRadius: "5px",
-    width: "300px",
-    height: "80px",
-  },
-  item: {
-    display: "inline-block",
-  },
-  skillsDisplay: {
-    justifyContent: "space-between",
-  },
-  level: {
-    fontSize: "40px",
-    textAlign: "center",
-  },
-  levelMobile: {
-    fontSize: "24px",
-    textAlign: "center",
-  },
-  icon: {
+  opIconArea: {
+    gridArea: "img",
     width: "60px",
     height: "60px",
-  },
-  iconMobile: {
-    width: "30px",
-    height: "30px",
-  },
-  opIcon: {
-    width: "160px",
-    height: "160px",
+    marginRight: "4px"
   },
   fav: {
-    fontSize: "32px",
-  },
-  favMobile: {
+    gridArea: "img",
     fontSize: "18px",
+  },
+  opNameArea: {
+    gridArea: "name",
+  },
+  opInfoArea: {
+    gridArea: "info",
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 1fr) 12px repeat(3, 1fr)",
+  },
+  opInfoIcon: {
+    fontSize: "24px",
+    textAlign: "center",
+    width: "30px",
+    height: "30px",
   },
   opName: {
     fontSize: "24px",
@@ -65,23 +49,33 @@ const useStyles = makeStyles({
   alterTitle: {
     fontSize: "14px",
   },
-  gapRight: {
-    marginRight: "20px",
+  raritySix: {
+    boxShadow: "0px 3px #f96601",
   },
+  rarityFive: {
+    boxShadow: "0px 3px #fbae02",
+  },
+  rarityFour: {
+    boxShadow: "0px 3px #dbb1db",
+  },
+  rarityThree: {
+    boxShadow: "0px 3px #00b2f6",
+  },
+  rarityTwo: {
+    boxShadow: "0px 3px #dce537",
+  },
+  rarityOne: {
+    boxShadow: "0px 3px #9f9f9f",
+  }
 });
 
 interface Props {
   op: Operator;
 }
 
-const OperatorCollectionBlock = ((props: Props) => {
+const OperatorCollectionBlock = React.memo((props: Props) => {
   const { op } = props;
   const classes = useStyles();
-  const potentialUrl = `https://res.cloudinary.com/samidare/image/upload/v1/arknights/potential/${op.potential}`;
-  const promotionUrl = `https://res.cloudinary.com/samidare/image/upload/v1/arknights/elite/${op.promotion}`;
-
-  //const size: Size = useWindowSize();
-  //const width = size.width === undefined ? 1920 : size.width;
 
   let intermediate = op.name;
   if (op.promotion === 2) {
@@ -90,16 +84,12 @@ const OperatorCollectionBlock = ((props: Props) => {
     intermediate += " elite 1";
   }
 
+  const potentialUrl = `https://res.cloudinary.com/samidare/image/upload/v1/arknights/potential/${op.potential}`;
+  const promotionUrl = `https://res.cloudinary.com/samidare/image/upload/v1/arknights/elite/${op.promotion}`;
   const opImgUrl = `https://res.cloudinary.com/samidare/image/upload/v1/arknights/operators/${slugify(
     intermediate,
-    { lower: true, replacement: "-", remove: /-/g }
+    { lower: true, replacement: "-", remove: /[-"]/g }
   )}`;
-
-  const opBoxStyleMobile = {
-    backgroundImage: `url("${opImgUrl}")`,
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "60px 60px",
-  }
 
   let opName = (
     <span className={classes.opName}>
@@ -120,106 +110,61 @@ const OperatorCollectionBlock = ((props: Props) => {
     )
   }
 
-  //if (width < MOBILE_BREAKPOINT) {
+  // merge operator portrait with rarity drop-shadow
+  const opIconStyle = clsx({
+    [classes.rarityOne]: op.rarity == 1,
+    [classes.rarityTwo]: op.rarity == 2,
+    [classes.rarityThree]: op.rarity == 3,
+    [classes.rarityFour]: op.rarity == 4,
+    [classes.rarityFive]: op.rarity == 5,
+    [classes.raritySix]: op.rarity == 6,
+    [classes.opIconArea]: true,
+  })
+
   return (
-    <div className={classes.opBoxMobile}>
-      <Box style={opBoxStyleMobile} className={classes.item} position="relative" height="80px" width="260px">
-        <Box position="absolute" left={64} top={-2}>
-          <div className={classes.opName}>{opName}</div>
-        </Box>
-        <Box position="absolute" right={0} top={30}>
-          <div className={classes.skillsDisplay}>
-            {(op.rarity > 2 ?
-              <SkillDisplayBox operator={op} skill={1} mobile={true} />
-              : <div />)}
-            {(op.rarity > 3 ?
-              <SkillDisplayBox operator={op} skill={2} mobile={true} />
-              : <div />)}
-            {(op.rarity === 6 || op.name === "Amiya" ?
-              <SkillDisplayBox operator={op} skill={3} mobile={true} />
-              : <div />)}
-          </div>
-        </Box>
-        <div style={{height: 60}} />
-        <div style={{ width: 60, height: 3, backgroundColor: COLOR_BY_RARITY[op.rarity], marginBottom: 3 }} />
-        <Box position="absolute" left={0} top={0}>
-          <div className={classes.favMobile}>{op.favorite ? "❤️" : ""}</div>
-        </Box>
-        <Box position="absolute" left={64} top={30}>
-          <img
-            src={promotionUrl}
-            className={classes.iconMobile}
-            alt={`Elite ${op.promotion} icon`}
-          />
-        </Box>
-        <Box position="absolute" left={80} top={30} width={60} height={60}>
-          <div className={classes.levelMobile}>{op.level}</div>
-        </Box>
-        <Box position="absolute" left={126} top={32}>
-          <img
-            src={potentialUrl}
-            className={classes.iconMobile}
-            alt={`Potential ${op.potential} icon`}
-          />
-        </Box>
-      </Box>
+    <div className={classes.opContainer}>
+      <img
+        src={opImgUrl}
+        className={opIconStyle}
+      />
+      <div className={classes.fav}>
+        {op.favorite ? "❤️" : ""}
+      </div>
+      <div className={classes.opNameArea}>
+        {opName}
+      </div>
+      <div className={classes.opInfoArea}>
+        <img
+          src={promotionUrl}
+          className={classes.opInfoIcon}
+          alt={`Elite ${op.promotion} icon`}
+        />
+        <span className={classes.opInfoIcon}>
+          {op.level}
+        </span>
+        <img
+          src={potentialUrl}
+          className={classes.opInfoIcon}
+          alt={`Potential ${op.potential} icon`}
+        />
+        <div/>
+        {(op.rarity > 2 ?
+          <span className={classes.opInfoIcon}>
+            <SkillDisplayBox operator={op} skill={1} />
+          </span>
+        : <div />)}
+        {(op.rarity > 3 ?
+          <span className={classes.opInfoIcon}>
+            <SkillDisplayBox operator={op} skill={2} />
+          </span>
+        : <div />)}
+        {(op.rarity === 6 || op.name === "Amiya" ?
+          <span className={classes.opInfoIcon}>
+            <SkillDisplayBox operator={op} skill={3} />
+          </span>
+        : <div />)}
+      </div>
     </div>
   );
-
-  //return (
-  //  <div className={classes.opBox}>
-  //    <Box className={classes.item} position="relative" height="160px" width="220px">
-  //      <img
-  //        className={classes.opIcon}
-  //        src={opImgUrl}
-  //        alt=""
-  //      />
-  //      <Box position="absolute" left={0} bottom={-4}>
-  //        <div style={{ width: 160, height: 4, backgroundColor: COLOR_BY_RARITY[op.rarity] }} />
-  //      </Box>
-  //      <Box position="absolute" left={0} top={0}>
-  //        <div className={classes.fav}>{op.favorite ? "❤️":""}</div>
-  //      </Box>
-  //      <Box position="absolute" right={0} bottom={0}>
-  //        <img
-  //            src={potentialUrl}
-  //            className={classes.icon}
-  //            alt={`Potential ${op.potential} icon`}
-  //        />
-  //      </Box>
-  //      <Box position="absolute" right={0} top={52} width={60} height={60}>
-  //        <div className={classes.level}>{op.level}</div>
-  //      </Box>
-  //      <Box position="absolute" right={0} top={0}>
-  //        <img
-  //            src={promotionUrl}
-  //            className={classes.icon}
-  //            alt={`Elite ${op.promotion} icon`}
-  //          />
-  //      </Box>
-  //    </Box>
-  //    <div className={classes.opName}>{opName}</div>
-  //    {(
-  //      <div className={classes.skillsDisplay}>
-  //        {(op.rarity > 2 ?
-  //          <span className={classes.gapRight}> 
-  //            <SkillDisplayBox operator={op} skill={1} />
-  //          </span>
-  //        : <div />)}
-  //        {(op.rarity > 3 ?
-  //          <span className={classes.gapRight}>
-  //            <SkillDisplayBox operator={op} skill={2} />
-  //          </span>
-  //        : <div />)}
-  //        {(op.rarity === 6 || op.name === "Amiya" ?
-  //          <span>
-  //            <SkillDisplayBox operator={op} skill={3} />
-  //          </span>
-  //        : <div />)}
-  //      </div>
-  //      )
-  //    }
-  //  </div>
-  //);
 });
 export default OperatorCollectionBlock;
