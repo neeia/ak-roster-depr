a5385327d602a389193e80aedb49abceb779a5aa
Neeia <neeia2718@gmail.com>
2022-03-25 17:02:44 -0700

    Simplify component

--- src/components/FormButton.tsx
+++ src/components/FormButton.tsx
@@ -14,17 +14,15 @@ const FormButton: React.FC<Props> = React.memo((props) => {
   const classes = useBoxStyles();
   const { children, onClick, toggled, disabled, className } = props;
 
-  const buttonStyle = clsx({
-    [classes.boxStyle]: true,
-    [classes.highlighted]: toggled,
-    [classes.disabled]: disabled,
-    [className ?? ""]: true,
-  })
-
   return (
     <ButtonBase
       onClick={onClick}
-      className={buttonStyle}
+      className={clsx({
+        [classes.boxStyle]: true,
+        [classes.highlighted]: toggled,
+        [classes.disabled]: disabled,
+        [className ?? ""]: true,
+      })}
       disabled={disabled}
     >
       {children}
