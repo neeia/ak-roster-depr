b3538ed8dcbd58cd07ded113d48bb09dd6b176df
Neeia <neeia2718@gmail.com>
2022-04-27 18:12:59 -0700

    Simplify logic

--- src/components/SkillDisplayBox.tsx
+++ src/components/SkillDisplayBox.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Operator } from "../App";
 import operatorJson from "../data/operators.json";
 import { makeStyles } from "@material-ui/core";
+import clsx from "clsx";
 
 interface Props {
   operator: Operator;
   skill: number;
+  className?: string;
 }
 
 const useStyles = makeStyles({
@@ -17,62 +19,57 @@ const useStyles = makeStyles({
     gridColumn: 1,
   },
   imageBG: {
-    height: "20px",
-    opacity: ".75",
+    opacity: ".8",
   },
   imageActive: {
-    height: "20px",
     opacity: ".95",
   },
   imageInactive: {
-    height: "20px",
     opacity: ".1",
   },
 });
 
-const SkillDisplayBox = React.memo((props : Props) => {
-  const { operator, skill } = props;
+const SkillDisplayBox = React.memo((props: Props) => {
+  const { operator, skill, className } = props;
   const classes = useStyles();
 
   const opInfo = (operatorJson as any)[operator.id];
-  const skillImgUrl = `https://res.cloudinary.com/samidare/image/upload/f_auto/v1/arknights/skills/${opInfo.skills[skill - 1].iconId
-    ?? opInfo.skills[skill - 1].skillId}`;
-  const skillLvlUrl = `https://res.cloudinary.com/samidare/image/upload/f_auto/v1/arknights/skill-levels/${operator.skillLevel}`;
-  const skillBGImgUrl = `https://res.cloudinary.com/samidare/image/upload/f_auto/v1/arknights/skill-levels/bg`;
+  const skillLvlUrl = `https://res.cloudinary.com/samidare/image/upload/f_auto,h_64,w_64/v1/arknights/skill-levels/${operator.skillLevel}`;
+  const skillBGImgUrl = `https://res.cloudinary.com/samidare/image/upload/f_auto,h_64,w_64/v1/arknights/skill-levels/bg`;
   const skillMastery = (operator as any)[`skill${skill}Mastery`];
-  const skillMasteryUrl = `https://res.cloudinary.com/samidare/image/upload/f_auto/v1/arknights/mastery/${skillMastery}`;
+  const skillMasteryUrl = `https://res.cloudinary.com/samidare/image/upload/f_auto,h_64,w_64/v1/arknights/mastery/${skillMastery}`;
+
+  const classBG = clsx({
+    [classes.skillImg]: true,
+    [classes.imageBG]: true,
+    [className ?? ""]: true,
+  })
+  const classImg = clsx({
+    [classes.skillImg]: true,
+    [classes.imageActive]: operator.promotion >= skill - 1,
+    [classes.imageInactive]: operator.promotion < skill - 1,
+    [className ?? ""]: true,
+  })
 
   return (
     <div className={classes.skillBox}>
-      {/*<div className={classes.skillImg}>
-        <img
-          className={classes.image}
-          src={skillImgUrl}
-          style={{ opacity: (operator.promotion >= skill - 1 ? 1 : 0.1) }}
-          alt={`Skill ${skill} ${opInfo.skills[0].skillName}`}
+      <img
+        className={classBG}
+        src={skillBGImgUrl}
+        alt={``}
+      />
+      {(!skillMastery || skillMastery === 0
+        ? <img
+          className={classImg}
+          src={skillLvlUrl}
+          alt={`Level ${operator.skillLevel}`}
         />
-      </div>*/}
-      <div className={classes.skillImg}>
-        <img
-          className={classes.imageBG}
-          src={skillBGImgUrl}
-          alt={``}
+        : <img
+          className={classImg}
+          src={skillMasteryUrl}
+          alt={`${skill} Mastery Level ${skillMastery}`}
         />
-      </div>
-      <div className={classes.skillImg}>
-        {(!skillMastery || skillMastery === 0
-          ? <img
-            className={operator.promotion >= skill - 1 ? classes.imageActive : classes.imageInactive}
-            src={skillLvlUrl}
-            alt={`Level ${operator.skillLevel}`}
-          />
-          : <img
-            className={operator.promotion >= skill - 1 ? classes.imageActive : classes.imageInactive}
-            src={skillMasteryUrl}
-            alt={`${skill} Mastery Level ${skillMastery}`}
-          />
-        )}
-      </div>
+      )}
     </div>
   );
 });
