8f7f5ffc92a2a368d70e4ecb7a1ec171df1535ce
Neia <neeia2718@gmail.com>
2022-07-12 02:35:37 -0700

    Implement modules, many Box sx changes

--- src/components/collectionTab/OperatorCollectionBlock.tsx
+++ src/components/collectionTab/OperatorCollectionBlock.tsx
@@ -1,103 +1,24 @@
 import React from "react";
 import { Operator } from "../../App";
-import { makeStyles } from "@material-ui/core";
-import { useRarityStyles } from "../StyleRarityUnderline";
+import { makeStyles, Hidden } from "@material-ui/core";
+import { useRarityStyles, rarityColors } from "../StyleRarityUnderline";
 import clsx from "clsx";
 import SkillDisplayBox from "./SkillDisplayBox";
 import { MdFavorite } from "react-icons/md";
 import { MAX_LEVEL_BY_RARITY } from "../RosterTable";
+import { Box } from "@mui/material";
 
 const useStyles = makeStyles({
-  opContainer: {
-    display: "grid",
-    gridTemplateAreas: `"name"
-                       "img"`,
-    gridTemplateRows: "auto 1fr",
-    padding: "6px",
-    border: "1px solid #808080",
-    borderRadius: "4px",
-    backgroundColor: "#40403E",
-    boxShadow: "2px 2px 8px rgb(0 0 0 / 30%)",
-    margin: "0px 14px 12px 14px",
-  },
   opNameArea: {
     gridArea: "name",
     display: "grid",
     gridTemplateColumns: "1fr auto"
-  },
-  opName: {
-    fontSize: "14px",
-    lineHeight: "20px",
-    marginLeft: "1px",
-  },
-  opNameSmaller: {
-    fontSize: "12px",
-    lineHeight: "20px",
-  },
-  alterName: {
-    fontSize: "9px",
-    lineHeight: "8px",
-  },
-  alterTitle: {
-    fontSize: "12px",
-    lineHeight: "12px",
-  },
-  opIconArea: {
-    gridArea: "img",
-    width: "120px",
-    height: "120px",
-  },
-  fav: {
-    alignSelf: "center",
-    justifySelf: "end",
   },
   imgArea: {
     gridArea: "img",
     display: "grid",
     gridTemplateAreas: `"level"`,
     position: "relative",
-  },
-  levelPromotionContainer: {
-    position: "absolute",
-    left: "-12px",
-    bottom: "-8px",
-    display: "grid",
-    gridTemplateAreas: `"potential" "elite" "level"`,
-  },
-  levelBubble: {
-    gridArea: "level",
-    width: "48px",
-    height: "48px",
-    fontSize: "26px",
-    lineHeight: "26px",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    marginLeft: "-4px",
-    marginBottom: "-2px",
-  },
-  levelBubbleLabel: {
-    gridArea: "level",
-    fontSize: "9px",
-    lineHeight: "3px",
-    display: "flex",
-    textDecoration: "none",
-    border: "none",
-  },
-  promotionBox: {
-    gridArea: "elite",
-    width: "20px",
-    height: "32px",
-    display: "flex",
-    alignItems: "end",
-    justifyContent: "center",
-    marginBottom: "4px",
-    marginLeft: "-2px",
-  },
-  promotionIcon: {
-    width: "32px",
-    height: "32px",
   },
   promotionBoxLabel: {
     gridArea: "elite",
@@ -112,74 +33,6 @@ const useStyles = makeStyles({
     border: "none",
     marginLeft: "-2px",
   },
-  potentialBox: {
-    display: "grid",
-    gridTemplateAreas: `"potential"`,
-    gridArea: "potential",
-    marginLeft: "3px",
-    marginBottom: "6px",
-    justifySelf: "start",
-  },
-  potentialSVG: {
-    gridArea: "potential",
-    width: "25px",
-    height: "28px",
-    alignSelf: "center",
-    justifySelf: "center",
-  },
-  potentialIcon: {
-    gridArea: "potential",
-    width: "24px",
-    height: "24px",
-    alignSelf: "center",
-    justifySelf: "center",
-  },
-  moduleContainer: {
-    gridArea: "img",
-    display: "flex",
-    flexDirection: "row-reverse",
-    alignSelf: "end",
-    marginBottom: "-4px",
-    marginRight: "-12px",
-  },
-  moduleSVG: {
-    display: "grid",
-    gridTemplateAreas: `"grid"`,
-    gridArea: "module",
-    width: "36px",
-    height: "36px",
-    alignSelf: "center",
-    justifySelf: "center",
-  },
-  moduleIcon: {
-    gridArea: "module",
-    width: "32px",
-    height: "32px",
-    alignSelf: "center",
-    justifySelf: "center",
-  },
-  opSkillArea: {
-    position: "absolute",
-    top: "4px",
-    right: "-16px",
-    display: "flex",
-    flexDirection: "column",
-    alignSelf: "center",
-    gap: "2px",
-    marginRight: "-4px"
-  },
-  skillIcon: {
-    height: "24px",
-  },
-  topSkill: {
-    marginLeft: "0px",
-  },
-  middleSkill: {
-    marginLeft: "4px",
-  },
-  bottomSkill: {
-    marginLeft: "8px",
-  },
 });
 
 interface Props {
@@ -189,7 +42,6 @@ interface Props {
 const OperatorCollectionBlock = React.memo((props: Props) => {
   const { op } = props;
   const classes = useStyles();
-  const rarity = useRarityStyles();
 
   let intermediate = op.id;
   if (op.promotion === 2) {
@@ -199,7 +51,6 @@ const OperatorCollectionBlock = React.memo((props: Props) => {
   }
 
   const potentialUrl = `/img/potential/${op.potential}.png`;
-  const promotionUrl = `/img/elite/${op.promotion}.png`;
   const opImgUrl = `/img/avatars/${intermediate}.png`;
 
   const reg = /( the )|\(/g;
@@ -207,174 +58,352 @@ const OperatorCollectionBlock = React.memo((props: Props) => {
   const name = nameSplitTitle.length > 1 ? nameSplitTitle[2].split(")")[0] : nameSplitTitle[0];
   const nameIsLong = name.split(" ").length > 1 && name.length > 11;
 
-  let opName = (
-    <span className={nameIsLong ? classes.opNameSmaller : classes.opName}>
-      {op.name}
-    </span>
-  )
+  let opName;
+  let splitName;
   if (op.name.includes(" the ")) {
-    const splitName = op.name.split(" the ");
-    opName = (
-      <span>
-        <div className={classes.alterName}>
-          {splitName[1]}
-        </div>
-        <div className={classes.alterTitle}>
-          {splitName[0]}
-        </div>
-      </span>
-    )
+    splitName = op.name.split(" the ");
   }
   if (op.name.includes(" (")) {
     const name = op.name.split(" (");
     const title = name[1].split(")");
+    splitName = [name, title]
+  }
+  if (splitName) {
     opName = (
       <span>
-        <div className={classes.alterName}>
-          {title[0]}
-        </div>
-        <div className={classes.alterTitle}>
-          {name[0]}
-        </div>
+        <Box sx={{
+          fontSize: { xs: "7px", sm: "9px" },
+          lineHeight: { xs: "6px", sm: "8px" },
+          marginLeft: "1px",
+        }}>
+          {splitName[1]}
+        </Box>
+        <Box sx={{
+          fontSize: { xs: "11px", sm: "12px" },
+          lineHeight: { xs: "11px", sm: "12px" },
+          marginLeft: "1px",
+        }}>
+          {splitName[0]}
+        </Box>
       </span>
+    )
+  } else {
+    opName = (
+      <Box sx={{
+        fontSize: { xs: nameIsLong ? "9px" : "12px", sm: nameIsLong ? "12px" : "14px" },
+        lineHeight: { xs: "17px", sm: "20px" },
+        marginLeft: "1px",
+      }}>
+        {op.name}
+      </Box>
     )
   }
 
   const potentialBlock =
-    <div className={classes.potentialBox}>
-      <svg
-        className={classes.potentialSVG}
-      >
-        <rect x="0" y="0" className={classes.potentialSVG}
-          fill="#323232" fillOpacity="0.95" stroke={op.potential === 6 ? "#ffffff" : "#808080"} strokeWidth="1.5" />
-      </svg>
-      <img
+    <Box sx={{
+      display: "grid",
+      gridTemplateAreas: `"potential"`,
+      gridArea: "potential",
+      marginLeft: "0px",
+      marginBottom: { xs: "2px", sm: "8px" },
+      justifySelf: "start",
+    }}>
+      <Box sx={{
+        gridArea: "potential",
+        width: { xs: "14px", sm: "25px" },
+        height: { xs: "20px", sm: "28px" },
+        alignSelf: "center",
+        justifySelf: "center",
+      }}>
+        <svg width="100%" height="100%">
+          <rect x="0" y="0" width="100%" height="100%"
+            fill="#323232" fillOpacity="0.95" stroke="#808080" strokeWidth="1.5" />
+        </svg>
+      </Box>
+      <Box
+        component="img"
+        sx={{
+          gridArea: "potential",
+          width: { xs: "16px", sm: "24px" },
+          height: { xs: "16px", sm: "24px" },
+          alignSelf: "center",
+          justifySelf: "center",
+        }}
         src={potentialUrl}
-        className={classes.potentialIcon}
         alt={`Potential ${op.potential}`}
       />
-    </div>
+    </Box >
 
-  const levelBubble =
-    <div className={classes.imgArea}>
-      <div className={classes.levelPromotionContainer}>
-        {potentialBlock}
-        <svg
-          className={classes.promotionBox}
-        >
-          <rect x="0" y="0" className={classes.promotionBox}
-            fill="#323232" fillOpacity="0.95" strokeWidth="2"
-            stroke={op.level === MAX_LEVEL_BY_RARITY[op.rarity][2] ? "#ffffff" : "#808080"} />
-        </svg>
-        <div className={classes.promotionBox}>
-          <img
-            src={promotionUrl}
-            className={classes.promotionIcon}
-            alt={``}
-          />
-        </div>
+  const promotionSx = {
+    display: "grid",
+    gridTemplateAreas: `"elite"`,
+    gridArea: "elite",
+    width: { xs: "12px", sm: "20px" },
+    height: { xs: "20px", sm: "32px" },
+    justifyContent: "center",
+    marginBottom: { xs: "6px", sm: "4px" },
+    marginLeft: { xs: "0px", sm: "-2px" },
+  }
+  const promotionBlock =
+    <Box sx={{ ...promotionSx }}>
+      <Hidden xsDown>
+        <Box sx={{
+          ...promotionSx
+        }} >
+          <svg width="100%" height="100%">
+            <rect x="0" y="0" width="100%" height="100%"
+              fill="#323232" fillOpacity="0.95" strokeWidth="2"
+              stroke="#808080" />
+          </svg>
+        </Box>
         <abbr
           className={classes.promotionBoxLabel}
           title={`E${op.promotion}`}
         >
           E{op.promotion}
         </abbr>
-        <svg
-          className={classes.levelBubble}
-        >
-          <circle cx="24" cy="24" r="22" className={classes.levelBubble}
+        <Box sx={{
+          ...promotionSx
+        }} >
+          <Box
+            component="img"
+            src={`/img/elite/${op.promotion}.png`}
+            sx={{
+              width: "32px",
+              height: "32px",
+            }}
+            alt={``}
+          />
+        </Box>
+      </Hidden>
+      <Hidden smUp>
+        <Box sx={{
+          ...promotionSx
+        }} >
+          <Box
+            component="img"
+            src={`/img/elite/${op.promotion}_s_box.png`}
+            sx={{
+              width: "26px",
+              height: "26px",
+            }}
+            alt={``}
+          />
+        </Box>
+      </Hidden>
+    </Box>
+
+  const levelSx = {
+    gridArea: "level",
+    display: "grid",
+    gridTemplateAreas: "level",
+    width: { xs: "32px", sm: "48px" },
+    height: { xs: "32px", sm: "48px" },
+    marginLeft: { xs: "-2px", sm: "-4px" },
+    marginBottom: "-2px",
+  };
+
+  const levelBlock =
+    <Box sx={{ ...levelSx }}>
+      <Box sx={{
+        ...levelSx
+      }} >
+        <svg width="100%" height="100%">
+          <circle cx="50%" cy="50%" r="45%"
             fill="#323232" fillOpacity="0.95" strokeWidth="2"
             stroke={op.level === MAX_LEVEL_BY_RARITY[op.rarity][2] ? "#f7d98b" : "#808080"} />
-        </svg >
-        <div className={classes.levelBubble} >
-          <abbr
-            className={classes.levelBubbleLabel}
-            title={`Level`}
+        </svg>
+      </Box>
+      <Box sx={{
+        ...levelSx,
+        fontSize: { xs: "18px", sm: "24px" },
+        lineHeight: { xs: "16px", sm: "24px" },
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }} >
+        <Hidden xsDown>
+          <Box
+            component="abbr"
+            title="Level"
+            sx={{
+              gridArea: "level",
+              fontSize: "9px",
+              lineHeight: "4px",
+              display: "flex",
+              textDecoration: "none",
+              border: "none",
+            }}
           >
             LV
-          </abbr>
-          {op.level}
-        </div >
-      </div >
-    </div>
+          </Box>
+        </Hidden>
+        {op.level}
+      </Box >
+    </Box >
+
+  const levelBubble =
+    <div className={classes.imgArea}>
+      <Box sx={{
+        gridArea: "level",
+        alignSelf: "end",
+        justifySelf: "start",
+        marginLeft: "-12px",
+        marginBottom: "-8px",
+        display: "grid",
+        gridTemplateAreas: `"potential" "elite" "level"`,
+      }}>
+        {op.potential > 1 ? potentialBlock : ""}
+        {op.promotion > 0 ? promotionBlock : ""}
+        {op.promotion > 0 || op.level > 1 ? levelBlock : ""}
+      </Box >
+    </div >
 
   const skillBlock =
     <div className={classes.imgArea}>
-      <div className={classes.opSkillArea}>
+      <Box sx={{
+        gridArea: "level",
+        marginTop: "4px",
+        marginRight: { xs: "-14px", sm: "-24px" },
+        display: "flex",
+        flexDirection: "column",
+        justifySelf: "end",
+        gap: "2px",
+      }}>
         {(op.rarity > 2 ?
-          <div className={classes.topSkill}>
-            <SkillDisplayBox operator={op} skill={1} className={classes.skillIcon} />
-          </div>
+          <Box sx={{ marginLeft: { xs: "0px", sm: "0px" } }}>
+            <SkillDisplayBox operator={op} skill={1} sx={{ height: { xs: "16px", sm: "24px" } }} />
+          </Box>
           : <div />)}
         {(op.rarity > 3 ?
-          <div className={classes.middleSkill}>
-            <SkillDisplayBox operator={op} skill={2} className={classes.skillIcon} />
-          </div>
+          <Box sx={{ marginLeft: { xs: "0px", sm: "4px" } }}>
+            <SkillDisplayBox operator={op} skill={2} sx={{ height: { xs: "16px", sm: "24px" } }} />
+          </Box>
           : <div />)}
         {(op.rarity === 6 || op.name === "Amiya" ?
-          <div className={classes.bottomSkill}>
-            <SkillDisplayBox operator={op} skill={3} className={classes.skillIcon} />
-          </div>
+          <Box sx={{ marginLeft: { xs: "0px", sm: "8px" } }}>
+            <SkillDisplayBox operator={op} skill={3} sx={{ height: { xs: "16px", sm: "24px" } }} />
+          </Box>
           : <div />)}
-      </div>
+      </Box>
     </div>
 
-  const opModuleUrls: string[] = op.module?.map((lvl: number) =>
-    lvl > 0 ? `/img/equip/uniequip_002_${op.id.split("_")[2]}.png` : ""
+  const opModuleUrls: string[] = op.module?.map((lvl: number, n: number) =>
+    lvl > 0 ? `/img/equip/uniequip_00${n + 2}_${op.id.split("_")[2]}.png` : ""
   ) ?? []
+  const opModuleStages: number[] = op.module?.map((lvl: number) => lvl) ?? []
   const moduleBlock =
-    <div className={classes.moduleContainer}>
-      {opModuleUrls.map((url: string) =>
+    <Box sx={{
+      gridArea: "img",
+      display: "flex",
+      flexDirection: "row-reverse",
+      alignSelf: "end",
+      marginBottom: { xs: "-4px", sm: "-4px" },
+      marginRight: { xs: "-12px", sm: "-16px" },
+      gap: { xs: "2px", sm: "4px" },
+    }}>
+      {opModuleUrls.map((url: string, n: number) =>
         url
-          ? <div key={url} className={classes.moduleSVG}>
-            <svg
-              className={classes.moduleSVG}
-            >
-              <rect x="0" y="0" className={classes.moduleSVG}
-                fill="#323232" fillOpacity="0.95" strokeWidth="1"
-                stroke={"#808080"} />
-            </svg>
-            <img
-              className={classes.moduleIcon}
+          ? <Box
+            key={n}
+            sx={{
+              display: "grid",
+              gridTemplateAreas: `"module"`,
+              alignSelf: "center",
+              justifySelf: "center",
+              alignItems: "center",
+              justifyContent: "center",
+              marginBottom: `-${4 * n}px`
+            }}>
+            <Box sx={{
+              gridArea: "module",
+              width: { xs: "24px", sm: "32px" },
+              height: { xs: "24px", sm: "32px" },
+            }}>
+              <svg width="100%" height="100%">
+                <rect x="0" y="0" width="100%" height="100%"
+                  fill="#323232" fillOpacity="0.75" strokeWidth="1"
+                  stroke={"#808080"} />
+              </svg>
+            </Box>
+            <Box
+              component="img"
+              sx={{
+                gridArea: "module",
+                width: { xs: "24px", sm: "32px" },
+                height: { xs: "24px", sm: "32px" },
+              }}
               src={url}
-              alt={"Module Owned"}
+              alt={`Module ${n + 1}`}
             />
-          </div>
+            {opModuleStages[n] > 1
+              ? <Box
+                component="abbr"
+                title={`Stage ${opModuleStages[n]}`}
+                sx={{
+                  gridArea: "module",
+                  width: { xs: "24px", sm: "32px" },
+                  height: { xs: "24px", sm: "32px" },
+                  textDecoration: "none",
+                  border: "none",
+                  fontSize: { xs: "16px", sm: "24px" },
+                  fontWeight: "bold",
+                  textAlign: "right",
+                  color: "black",
+                  WebkitTextFillColor: "white",
+                  WebkitTextStroke: "1px black",
+                  marginBottom: "-4px",
+                  marginLeft: "-2px"
+                }}
+              >
+                {opModuleStages[n]}
+              </Box> : ""}
+          </Box>
           : "")}
-    </div>
-
-  // merge operator portrait with rarity drop-shadow
-  const opIconStyle = clsx({
-    [rarity.rarityOne]: op.rarity === 1,
-    [rarity.rarityTwo]: op.rarity === 2,
-    [rarity.rarityThree]: op.rarity === 3,
-    [rarity.rarityFour]: op.rarity === 4,
-    [rarity.rarityFive]: op.rarity === 5,
-    [rarity.raritySix]: op.rarity === 6,
-    [classes.opIconArea]: true,
-  })
+    </Box >
 
   return (
-    <div className={classes.opContainer}>
+    <Box sx={{
+      display: "grid",
+      gridTemplateAreas: `"name" "img"`,
+      gridTemplateRows: "auto 1fr",
+      padding: { xs: "4px 8px 4px 6px", sm: "6px 10px 8px 10px" },
+      border: "1px solid #808080",
+      borderRadius: "4px",
+      backgroundColor: "#40403E",
+      boxShadow: "2px 2px 8px rgb(0 0 0 / 30%)",
+      margin: { xs: "2px 4px 4px 10px", sm: "2px 16px 10px 12px" },
+    }}>
       <div className={classes.opNameArea}>
         {opName}
         {op.favorite
-          ? <MdFavorite
-            className={classes.fav}
-            size={18}
-            color={"#ff4d4d"}
-          />
+          ? <Box sx={{
+            alignSelf: "center",
+            justifySelf: "end",
+            lineHeight: "0px",
+            marginRight: "-2px",
+          }}>
+            <MdFavorite
+              size={18}
+              color={"#ff4d4d"}
+            />
+          </Box>
           : ""}
       </div>
-      <img
+      <Box
+        component="img"
         src={opImgUrl}
-        className={opIconStyle}
-        alt=""
+        sx={{
+          gridArea: "img",
+          width: { xs: "80px", sm: "120px" },
+          borderBottom: `3px solid ${rarityColors[op.rarity]}`,
+        }}
       />
       {levelBubble}
       {skillBlock}
-      {moduleBlock}
-    </div>
+      {op.module ? moduleBlock : ""}
+    </Box>
   );
 });
 export default OperatorCollectionBlock;
--- src/components/collectionTab/SkillDisplayBox.tsx
+++ src/components/collectionTab/SkillDisplayBox.tsx
@@ -2,34 +2,22 @@ import React from "react";
 import { Operator } from "../../App";
 import { makeStyles } from "@material-ui/core";
 import clsx from "clsx";
+import { Box, SxProps, Theme } from "@mui/material";
 
 interface Props {
   operator: Operator;
   skill: number;
-  className?: string;
+  sx?: SxProps<Theme>;
 }
 
 const useStyles = makeStyles({
   skillBox: {
     display: "grid",
   },
-  skillImg: {
-    gridRow: 1,
-    gridColumn: 1,
-  },
-  imageBG: {
-    opacity: ".8",
-  },
-  imageActive: {
-    opacity: ".95",
-  },
-  imageInactive: {
-    opacity: ".1",
-  },
 });
 
 const SkillDisplayBox = React.memo((props: Props) => {
-  const { operator, skill, className } = props;
+  const { operator, skill, sx } = props;
   const classes = useStyles();
 
   const skillLvlUrl = `/img/rank/${operator.skillLevel}.png`;
@@ -37,33 +25,39 @@ const SkillDisplayBox = React.memo((props: Props) => {
   const skillMastery = (operator as any)[`skill${skill}Mastery`];
   const skillMasteryUrl = `/img/rank/m-${skillMastery}.png`;
 
-  const classBG = clsx({
-    [classes.skillImg]: true,
-    [classes.imageBG]: true,
-    [className ?? ""]: true,
-  })
-  const classImg = clsx({
-    [classes.skillImg]: true,
-    [classes.imageActive]: operator.promotion >= skill - 1,
-    [classes.imageInactive]: operator.promotion < skill - 1,
-    [className ?? ""]: true,
-  })
-
   return (
     <div className={classes.skillBox}>
-      <img
-        className={classBG}
+      <Box
+        component="img"
+        sx={{
+          gridRow: 1,
+          gridColumn: 1,
+          opacity: ".8",
+          ...sx
+        }}
         src={skillBGImgUrl}
         alt={``}
       />
       {(!skillMastery || skillMastery === 0
-        ? <img
-          className={classImg}
+        ? <Box
+          component="img"
+          sx={{
+            gridRow: 1,
+            gridColumn: 1,
+            opacity: operator.promotion >= skill - 1 ? ".95" : "0.1",
+            ...sx
+          }}
           src={skillLvlUrl}
           alt={`Level ${operator.skillLevel}`}
         />
-        : <img
-          className={classImg}
+        : <Box
+          component="img"
+          sx={{
+            gridRow: 1,
+            gridColumn: 1,
+            opacity: operator.promotion >= skill - 1 ? ".95" : "0.1",
+            ...sx
+          }}
           src={skillMasteryUrl}
           alt={`${skill} Mastery Level ${skillMastery}`}
         />
